e8bd4b821d436e9b04662d27fd7ab912da284724
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-03-27 12:38:15 +0100

    [feat] chap8: ajout de style sur le textInput et le bouton de la vue Search

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
 
 class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeHolder='Titre du film' />
-                <Button title='Rechercher' onPress={() => {}} />
+            <View style={{ marginTop: 20 }}>
+                <TextInput style={ styles.textinput } placeHolder="Titre du film" />
+                <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    textinput: {
+      marginLeft: 5,
+      marginRight: 5,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5
+    }
+})
 
 export default Search;

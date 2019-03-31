32c865c81f9a5d9e27bad6342d76017db8728bcf
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-03-31 17:46:58 +0200

    [feat] chap10: ajout filmItem, utilisation props dans FlatList

--- Components/Search.js
+++ Components/Search.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList  } from 'react-native'
 import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
+
 
 class Search extends React.Component {
-    render() {
-        return (
-            <View style={ styles.main_container }>
-                <TextInput style={ styles.textinput } placeHolder="Titre du film" />
-                <Button title="Rechercher" onPress={() => {}} />
-                <FlatList
-                    data={films}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <Text>{item.title}</Text>}
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button title='Rechercher' onPress={() => {}}/>
+        <FlatList
+            data={films}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <FilmItem/>}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        marginTop: 20, 
-        flex: 1,
-        backgroundColor: 'grey'
-    },
-    textinput: {
-      marginLeft: 5,
-      marginRight: 5,
-      height: 50,
-      borderColor: '#000000',
-      borderWidth: 1,
-      paddingLeft: 5
-    }
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
 })
 
-export default Search;
+export default Search

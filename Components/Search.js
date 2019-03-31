4a93c76185687176e5305288e155d72636f7e684
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-03-31 19:23:46 +0200

    [feat] chap12: ajout mire d'attente lors de la recherche des films

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList  } from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' 
 
@@ -10,13 +10,29 @@ class Search extends React.Component {
         this.searchedText = ""
         this.state = { 
             films: [],
+            isLoading: false
         }
     }
 
     _loadFilms() {
+        this.setState({ isLoading: true })
         if (this.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.searchedText)
-                .then(data => this.setState( { films: data.results } ))
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => 
+                this.setState({ 
+                    films: data.results,
+                    isLoading: false 
+                })
+            )
+        }
+    }
+
+    _displayLoad() {
+        if (this.state.isLoading) {
+            return (
+            <View style={styles.loading_container}>
+                <ActivityIndicator size='large' />
+            </View>
+            )
         }
     }
 
@@ -36,6 +52,7 @@ class Search extends React.Component {
             keyExtractor={(item) => item.id.toString()}
             renderItem={({item}) => <FilmItem film={item}/>}
         />
+        {this._displayLoad()}
       </View>
     )
   }
@@ -53,6 +70,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 

e95b377246e784c59752b2ae03f9f6b4a66d9cdc
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-03-31 19:29:16 +0200

    [style] indentation et nettoyage des fichiers non utilisés

--- Components/Search.js
+++ Components/Search.js
@@ -42,44 +42,44 @@ class Search extends React.Component {
 
   render() {
     return (
-      <View style={styles.main_container}>
-        <TextInput onSubmitEditing={() => this._loadFilms()} 
-                   onChangeText={(text) => this._searchTextInputChanged(text)} 
-                   style={styles.textinput} placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
-        <FlatList
-            data={this.state.films}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({item}) => <FilmItem film={item}/>}
-        />
-        {this._displayLoad()}
-      </View>
+        <View style={styles.main_container}>
+            <TextInput onSubmitEditing={() => this._loadFilms()} 
+                    onChangeText={(text) => this._searchTextInputChanged(text)} 
+                    style={styles.textinput} placeholder='Titre du film'/>
+            <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+            <FlatList
+                data={this.state.films}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => <FilmItem film={item}/>}
+            />
+            {this._displayLoad()}
+        </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    flex: 1,
-    marginTop: 20
-  },
-  textinput: {
-    marginLeft: 5,
-    marginRight: 5,
-    height: 50,
-    borderColor: '#000000',
-    borderWidth: 1,
-    paddingLeft: 5
-  },
-  loading_container: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 100,
-    bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    main_container: {
+        flex: 1,
+        marginTop: 20
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default Search

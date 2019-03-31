ee201de04c08809c2180a960e02fa14473a8f3ed
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-03-31 19:11:03 +0200

    [feat] chap12: ajout de l'image du film à partir du champ poster_path de l'api tmdb

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -3,9 +3,13 @@
 //import API_TOKEN from '../Helpers/token'
 const API_TOKEN = "80bef6a0b5f84c0f819ff30c22497b68"
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
-    return fetch(url)
-      .then((response) => response.json())
-      .catch((error) => console.error(error))
-  }
+export function getFilmsFromApiWithSearchedText(text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}

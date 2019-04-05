2a1ab74109898d840be145eee671a7a1e6ad228d
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2019-04-05 15:20:46 +0200

    [feat] chap19: creation d'une barre de navigation de type bottomTabNavigator; ajout vue favoris

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,8 +1,11 @@
 // Navigation/Navigation.js
 
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import React from 'react'
+import { StyleSheet, Image} from 'react-native'
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
+import Favorites from '../Components/Favorites'
 
 const SearchStackNavigator = createStackNavigator({
   Search: { 
@@ -16,4 +19,41 @@ const SearchStackNavigator = createStackNavigator({
   }
 })
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator({
+  Search: {
+    screen: SearchStackNavigator,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image
+          source={require('../Images/ic_search.png')}
+          style={styles.icon} />
+      }
+    }
+  },
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image
+          source={require('../Images/ic_favorite.png')}
+          style={styles.icon} />
+      }
+    }
+  }
+}, {
+  tabBarOptions: {
+    showLabel: false,
+    showIcon: true,
+    activeBackgroundColor: '#DDDDDD',
+    inactiveBackgroundColor: '#FFFFFF'
+  }
+})
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)

812d1c35734f489914282460fb4796d2ecaac8f9
Unknown <devilleduc.thomas@gmail.com>
2019-04-08 18:15:06 +0200

    [feat] ajout component avatar; integration au componant favorites

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -1,24 +1,37 @@
 // Components/Favorites.js
 
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import FilmList from './FilmList'
+import Avatar from './Avatar'
 import { connect } from 'react-redux'
 
 class Favorites extends React.Component {
 
   render() {
     return (
-      <FilmList
-        films={this.props.favoritesFilm}
-        navigation={this.props.navigation}
-        favoriteList={true}
-      />
+      <View style={styles.main_container}>
+        <View style={styles.avatar_container}>
+          <Avatar/>
+        </View>
+        <FilmList
+          films={this.props.favoritesFilm}
+          navigation={this.props.navigation}
+          favoriteList={true}
+        />
+      </View>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1
+  },
+  avatar_container: {
+    alignItems: 'center'
+  }
+})
 
 const mapStateToProps = state => {
   return {

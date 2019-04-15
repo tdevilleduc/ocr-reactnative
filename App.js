5ff80f099c5ff509a9d89708bcace746db772f1c
Unknown <devilleduc.thomas@gmail.com>
2019-04-15 18:48:49 +0200

    [feat] ajout de persistence à l'application

--- App.js
+++ App.js
@@ -3,13 +3,18 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/es/integration/react'
 import Store from './Store/configureStore'
+import { persistStore } from 'redux-persist'
 
 export default class App extends React.Component {
   render() {
+    let persistor = persistStore(Store)
     return (
       <Provider store={Store}>
-        <Navigation/>
+        <PersistGate persistor={persistor}>
+          <Navigation/>
+        </PersistGate>
       </Provider>
     )
   }
